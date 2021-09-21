aa0ecfff5288b0446eb358915cec88858b3aa18c
Tim Januario <timothy.januario@ucsf.edu>
2021-09-21 14:38:59 -0400

    Added two new null resources

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 resource "null_resource" "null_object" {
 }
 
-#resource "null_resource" "null_object2" {
-#}
+resource "null_resource" "null_object_another" {
+}
+
+resource "null_resource" "null_object_yet_another" {
+}
